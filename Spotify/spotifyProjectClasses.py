5665a402449b5bbea6f509b3e66ede5b83856c34
AlejandroGarciaGeophysics <48917652+AlejandroGarciaGeophysics@users.noreply.github.com>
2019-03-26 14:08:10 -0400

    Updated the file to include cSim
    CosineSimilarity

--- Spotify/spotifyProjectClasses.py
+++ Spotify/spotifyProjectClasses.py
@@ -4,12 +4,10 @@ Created on Mon Feb 25 15:57:40 2019
 
 @author: agarc
 """
-
-#test
-
 import numpy as np
 import matplotlib.pyplot as plt
-import sklearn
+import sklearn.metrics
+import sklearn.preprocessing
 import os
 
 class musicFan:
@@ -22,13 +20,13 @@ class musicFan:
         
         self.ID = ID
         self.session = np.int_(tastes[11])
-        self.dataID = tastes[13]
+        self.dataID = np.array(tastes[13],dtype = "str")
         self.songID = tastes[14]
         self.response = np.int_(tastes[12])
-        self.tastes = [np.float64(tastes[0]), np.float64(tastes[1]), np.int_(tastes[2])\
+        self.tastes = np.array([np.float64(tastes[0]), np.float64(tastes[1]), np.int_(tastes[2])\
                        , np.float64(tastes[3]), np.int_(tastes[4]), np.float64(tastes[5]),\
                        np.float64(tastes[6]), np.float64(tastes[7]), np.float64(tastes[8])\
-                       , np.float64(tastes[9]), np.float64(tastes[10])]
+                       , np.float64(tastes[9]), np.float64(tastes[10])]).T
         
         if not os.path.exists("UserOutput"):
             os.makedirs("UserOutput")
@@ -85,23 +83,36 @@ class musicFan:
             plt.savefig("UserOutput/User"+str(self.ID)+"/histograms/"+self.keys[i]+"_histogram"+".png")
             plt.close("all")
             
+    def cosineSimilarity(self,songs):
+        a = sklearn.preprocessing.normalize(self.tastes[self.response == 1],axis=0)
+        b = sklearn.preprocessing.normalize(songs.features,axis=0)
+        self.cSim = sklearn.metrics.pairwise.cosine_similarity(a,b)
+        print("Cosinesimilarity successful for user:",self.ID,".")
+        
             
         
-class song:
+class songs:
 
     keys = ["danceability","energy","key","loudness","mode","speechiness","acousticness","instrumentalness"\
             ,"liveness","valence","tempo"]
     
     def __init__(self,features):
         
-        self.ID = features[18]
-        self.name = features[19]
-        self.albumID = features[2]
-        self.albumName = features[3]
-        self.artistID = features[5]
-        self.artistName = features[6]
-        self.year = features[26]
-        self.features = [np.float64(features[7]), np.float64(features[10]), np.float64(features[12]), np.float(features[14]),\
-                                    np.int_(features[15]), np.float64(features[20]), np.float64(features[1]),\
-                                    np.float64(features[11]),np.float64(features[13]),np.float64(features[25]),np.float64(features[21])]
+        self.ID = features[:,18]
+        self.name = features[:,19]
+        self.albumID = features[:,2]
+        self.albumName = features[:,3]
+        self.artistID = features[:,5]
+        self.artistName = features[:,6]
+        self.year = features[:,26]
+        self.features = np.array([np.float64(features[:,7]), np.float64(features[:,10]), np.float64(features[:,12]), np.float64(features[:,14]),\
+                                    np.int_(features[:,15]), np.float64(features[:,20]), np.float64(features[:,1]),\
+                                    np.float64(features[:,11]),np.float64(features[:,13]),np.float64(features[:,25]),np.float64(features[:,21])]).T
       
+    def recommend(self,user):
+        
+        a = np.concatenate([self.ID[user.cSim[i]>0.95] for i in range(len(user.cSim))])
+                
+        print(a.shape)
+        #return a    
+        return np.unique(a, return_counts = True)
